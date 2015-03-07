2e694df5aa05b041979e32c96a340914fd073cb8
Kathryn Killebrew <kathryn.killebrew@gmail.com>
2015-03-07 11:02:44 -0500

    Use grunt/bower/node to manage packages

---
+++ Gruntfile.js
@@ -0,0 +1,37 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        compass: {                  // Task
+            dist: {                   // Target
+              options: {              // Target options
+                sassDir: '_sass',
+                cssDir: 'css',
+                environment: 'production'
+              }
+            },
+            dev: {                    // Another target
+              options: {
+                sassDir: '_sass',
+                cssDir: 'css'
+              }
+            }
+        },
+        watch: {
+            css: {
+                files: '**/*.scss',
+                tasks: ['compass']
+            }
+        },
+        uglify: {
+            my_target: {
+              files: {
+                'js/impress.min.js': ['bower_components/impress.js/js/impress.js']
+              }
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-compass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.registerTask('default', ['uglify', 'compass', 'watch']);
+};
