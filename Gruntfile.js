b99e61ab0cde30492886db92d0b36dd310f1222a
Kathryn Killebrew <kathryn.killebrew@gmail.com>
2015-03-11 16:38:13 -0700

    Remove rest of video support

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,18 +7,6 @@ module.exports = function(grunt) {
                 src: ['!css/**', 'css/*', '!*/syntax.css', 'js/*', 'img/*']
             }
         },
-        concat: {
-            jquery: {
-                files: {
-                    'js/jquery.min.js': ['bower_components/jquery/jquery.min.js']
-                }
-            },
-            jquerymap: {
-                files: {
-                    'js/jquery.min.map': ['bower_components/jquery/jquery.min.map']
-                }
-            }
-        },
         compass: {                  // Task
             dist: {                   // Target
               options: {              // Target options
@@ -52,21 +40,17 @@ module.exports = function(grunt) {
         uglify: {
             dist: {
               files: {
-                'js/impress.min.js': ['bower_components/impress.js/js/impress.js'],
-                'js/video-fallback.min.js': ['_js/video-fallback.js']
+                'js/impress.min.js': ['bower_components/impress.js/js/impress.js']
               }
             }
         }
     });
-    grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-newer');
     grunt.loadNpmTasks('grunt-contrib-imagemin');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-compass');
     grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.registerTask('concat-bower', ['concat:jquery', 'concat:jquerymap']);
-    grunt.registerTask('default', ['clean:dist', 'compass', 'uglify', 'imagemin', 'concat-bower']);
-    grunt.registerTask('update', ['newer:compass', 'newer:uglify', 'newer:imagemin',
-                                  'newer:concat-bower']);
+    grunt.registerTask('default', ['clean:dist', 'compass', 'uglify', 'imagemin']);
+    grunt.registerTask('update', ['newer:compass', 'newer:uglify', 'newer:imagemin']);
 };
